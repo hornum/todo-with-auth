6682d612b6926dcc570ed44408c4d621ffad645a
hornum <a.chere.pnin@mail.ru>
2026-03-12 19:24:48 +0300

    Add database file with db connection

--- app/database.py
+++ app/database.py
@@ -1,1 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from config import settings
+
+
+DATABASE_URL = settings.database_url
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
