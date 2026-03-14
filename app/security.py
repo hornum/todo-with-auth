d4a4e89e70bbe3cd35aa6695f46c8e1dfdf509b2
hornum <a.chere.pnin@mail.ru>
2026-03-14 22:38:22 +0300

    Fix switch to pwdlib as recommended

--- app/security.py
+++ app/security.py
@@ -1,7 +1,7 @@
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = PasswordHash.recommended()
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
