c98d0588a4d48b59274b9135003b1c963fa42e8d
hornum <a.chere.pnin@mail.ru>
2026-03-30 18:43:37 +0300

    Add Token schema Add CreateTodo schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,22 @@
-from pydantic import BaseModel, EmailStr
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+
 
 class UserRegister(BaseModel):
     email: EmailStr
     username: str
     password: str
+    is_superuser: bool
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class CreateTodo(BaseModel):
+    title: str = Field(max_length=200)
+    description: Optional[str] = Field(max_length=300)
+    is_done: Optional[bool] = False
+    priority: int = Field(gt=0, lt=6)
+
+class TodoStatusUpdate(BaseModel):
+    is_done: bool
