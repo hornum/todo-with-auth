bc850dfca82df2585706256440ec00c27ae1ad16
hornum <a.chere.pnin@mail.ru>
2026-03-30 18:45:53 +0300

    Delete main() func which returned "Hello": "World"

--- app/main.py
+++ app/main.py
@@ -2,14 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.routers.auth import router as auth_router
+from app.routers.tasks import router as tasks_router
 
 
 app = FastAPI()
 app.include_router(auth_router)
-
-@app.get("/")
-async def main():
-    return {"Hello": "World"}
-
-if __name__ == "__main__":
-    uvicorn.run('main:app', port=8000, reload=True)
+app.include_router(tasks_router)
