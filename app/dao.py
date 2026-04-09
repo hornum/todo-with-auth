67dcbd57093a9bd8cafa3c09a6d751596fea9e96
hornum <a.chere.pnin@mail.ru>
2026-04-09 20:52:29 +0300

    Small refactoring

--- app/dao.py
+++ app/dao.py
@@ -1,32 +1,29 @@
-from typing import List
-
-from sqlalchemy import select, delete
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import admin_service
 from app.models import Task, User
 from app.schemas import TaskCreate
 
 
-async def get_user_by_id(db: AsyncSession, user_id: int) -> User| None:
+async def get_user_by_id(db: AsyncSession, user_id: int) -> User | None:
     stmt = select(User).where(User.id == user_id)
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
 
 async def add_task(db: AsyncSession, user_id: int, task: TaskCreate) -> Task:
     new_task = Task(
-            title=task.title,
-            description=task.description,
-            user_id=user_id,
-            priority=task.priority,
-            is_completed=task.is_completed,
+        title=task.title,
+        description=task.description,
+        user_id=user_id,
+        priority=task.priority,
+        is_completed=task.is_completed,
     )
     db.add(new_task)
     await db.commit()
     await db.refresh(new_task)
     return new_task
 
-async def get_all_tasks_for_user(db: AsyncSession, user_id: int | None) -> List[Task]:
+async def get_all_tasks_for_user(db: AsyncSession, user_id: int) -> list[Task]:
     stmt = select(Task).where(Task.user_id == user_id)
     result = await db.execute(stmt)
     return result.scalars().all()
@@ -36,7 +33,7 @@ async def get_task_by_id(db: AsyncSession, task_id: int) -> Task | None:
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
 
-async def get_all_tasks(db: AsyncSession) -> List[Task]:
+async def get_all_tasks(db: AsyncSession) -> list[Task]:
     stmt = select(Task)
     result = await db.execute(stmt)
     return result.scalars().all()
