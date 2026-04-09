cc057de81e9732b6e184cd8e634ee6769a3b9f58
hornum <a.chere.pnin@mail.ru>
2026-04-09 20:49:43 +0300

    Add /api/v1 prefix

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.routers.users import router as users_router
 
 
 app = FastAPI()
-app.include_router(auth_router)
-app.include_router(tasks_router)
-app.include_router(admin_router)
-app.include_router(users_router)
+app.include_router(auth_router, prefix="/api/v1")
+app.include_router(tasks_router, prefix="/api/v1")
+app.include_router(admin_router, prefix="/api/v1")
+app.include_router(users_router, prefix="/api/v1")
