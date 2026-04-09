659d59cc8c1476bb68bb8d8b7c99464ed3ccff0f
hornum <a.chere.pnin@mail.ru>
2026-04-09 22:32:22 +0300

    Refactor as now user dependency returns user object, not dict

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -3,36 +3,34 @@ from typing import Annotated
 
 import app.service as service
 from app.database import db_dependency
-from app.schemas import CreateTodo, TodoStatusUpdate, TaskResponse
+from app.models import User
 from app.routers.auth import get_current_user
 from app.schemas import TaskCreate, TaskResponse, TaskStatusUpdate
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
-user_dependency = Annotated[dict, Depends(get_current_user)]
+user_dependency = Annotated[User, Depends(get_current_user)]
 
 @router.get("/", response_model=list[TaskResponse])
 async def get_all_tasks(db: db_dependency, current_user: user_dependency):
-    return await service.get_all_tasks(db, current_user.get("user_id"))
+    return await service.get_all_tasks(db, current_user.id)
 
 @router.get("/{task_id}", response_model=TaskResponse)
 async def get_task_by_id(db: db_dependency, current_user: user_dependency, task_id: int):
-    return await service.get_task_by_id(db, current_user.get("user_id"), task_id)
+    return await service.get_task_by_id(db, current_user.id, task_id)
 
 @router.post("/", response_model=TaskResponse, status_code=201)
-async def create_task(db: db_dependency, current_user: user_dependency, task: CreateTodo):
-    return await service.create_task(db, current_user.get("user_id"), task)
 async def create_task(db: db_dependency, current_user: user_dependency, task: TaskCreate):
+    return await service.create_task(db, current_user.id, task)
 
 @router.patch("/{task_id}/status", response_model=TaskResponse)
-async def update_task(db: db_dependency, current_user: user_dependency, task_id: int, status: TodoStatusUpdate):
-    return await service.update_task_status(db, current_user.get("user_id"), task_id, status)
 async def update_task(db: db_dependency, current_user: user_dependency, task_id: int, status: TaskStatusUpdate):
+    return await service.update_task_status(db, current_user.id, task_id, status)
 
 @router.delete("/all", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_all_tasks(db: db_dependency, current_user: user_dependency):
-    return await service.delete_all_user_tasks(db, current_user.get("user_id"))
+    return await service.delete_all_user_tasks(db, current_user.id)
 
 @router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(db: db_dependency, current_user: user_dependency, task_id: int):
-    return await service.delete_task_by_id(db, current_user.get("user_id"), task_id)
+    return await service.delete_task_by_id(db, current_user.id, task_id)
--- app/routers/users.py
+++ app/routers/users.py
@@ -3,21 +3,19 @@ from typing import Annotated
 
 import app.service as service
 from app.database import db_dependency
-from app.schemas import UserResponse, ChangePassword
+from app.models import User
 from app.routers.auth import get_current_user
 from app.schemas import PasswordChange, UserResponse
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-user_dependency = Annotated[dict, Depends(get_current_user)]
-# get_password_hash
+user_dependency = Annotated[User, Depends(get_current_user)]
 
 @router.get("/me", response_model=UserResponse)
 async def get_user(db: db_dependency, user: user_dependency):
-    return await service.get_user(db, user.get("user_id"))
+    return await service.get_user(db, user.id)
 
 @router.post("/password", status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(db: db_dependency, user: user_dependency, password_verify: ChangePassword):
-    return await service.change_password(db, user.get("user_id"), password_verify)
 async def change_password(db: db_dependency, user: user_dependency, password_verify: PasswordChange):
+    return await service.change_password(db, user.id, password_verify)
